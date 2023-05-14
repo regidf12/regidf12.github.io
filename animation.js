4160ba4d6329599d0bca00eb0a13b8dc1dead485
White-prince <tiltedfear@gmail.com>
2023-05-14 18:21:35 +0300

    add alert of js

--- animation.js
+++ animation.js
@@ -61,3 +61,12 @@ const wrap1 = new parallaxTiltEffect({
   element: $('.wrap--1'),
   tiltEffect: 'reverse'
 });
+<script src="http://code.jquery.com/jquery.min.js"></script>
+2
+<script>
+3
+if($) alert("Yes!") ;
+4
+else alert("No") ;
+5
+</script>
